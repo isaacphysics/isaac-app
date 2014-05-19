2a66da2a371ab5428d89664787f91ae902cb1df7
Ian Davies <git@iandavies.org>
2014-05-19 15:32:42 +0100

    Add Sass compilation with Grunt.

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -15,6 +15,12 @@ define(["foundation", "angular", "angular-route", "app/controllers", "app/direct
 		$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
 		$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
 		$routeProvider.otherwise({redirectTo: '/view1'});
+	}])
+
+	.run([function() {
+
+		$(document).foundation();
+
 	}]);
 
 
