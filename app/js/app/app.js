2e7e5e7dad2711dfb60491378ad1dff293b3cb0b
Ian Davies <git@iandavies.org>
2014-05-21 18:20:49 +0100

    Switch Angular JS into html5 Mode.

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -11,7 +11,10 @@ define(["foundation", "angular", "angular-route", "app/controllers", "app/direct
 		'isaac.controllers'
 	])
 
-	.config(['$routeProvider', function($routeProvider) {
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+		$locationProvider.html5Mode(true).hashPrefix("!");
+
 		$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
 		$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
 		$routeProvider.otherwise({redirectTo: '/view1'});
