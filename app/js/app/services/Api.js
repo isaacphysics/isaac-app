309e01da6a1203ee519443688c2540b17e02da0d
Stephen Cummins <sacummins@gmail.com>
2014-05-30 16:21:22 +0100

    Changed endpoints from isaac/api to /api

--- app/js/app/services/Api.js
+++ app/js/app/services/Api.js
@@ -4,11 +4,11 @@ define([], function() {
 	
 	var Api = function ApiConstructor($resource, server) {
 
-		this.pages = $resource(server + "/isaac/api/pages/:id");
-		this.content = $resource(server + "/isaac/api/pages/:id"); // TODO: Use the actual content endpoint once it is written.
+		this.pages = $resource(server + "/api/pages/:id");
+		this.content = $resource(server + "/api/pages/:id"); // TODO: Use the actual content endpoint once it is written.
 		
 		var conceptsPerPage = 10;
-		var conceptList = $resource(server + "/isaac/api/concepts?start_index=:startIndex&limit=:limit");
+		var conceptList = $resource(server + "/api/concepts?start_index=:startIndex&limit=:limit");
 
 		this.getConceptList = function(page){
 			//page = parseInt(page) -1;
@@ -17,12 +17,12 @@ define([], function() {
 		}
 
 		this.getImageUrl = function(path) {
-			return server + "/isaac/api/images/" + path;
+			return server + "/api/images/" + path;
 		}
 
 		this.admin = {
 			synchroniseDatastores: function() {
-				$.post(server + "/isaac/admin/synchronise_datastores").then(function() {
+				$.post(server + "/api/admin/synchronise_datastores").then(function() {
 					console.warn("Synchronising Datastores. The next page load will take a while.");
 				})
 			}
