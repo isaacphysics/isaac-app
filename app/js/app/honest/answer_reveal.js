04cc0adcb33839737f90cbefea5ae75644109bab
Stephen Cummins <sacummins@gmail.com>
2014-06-11 15:40:06 +0100

    Updated answer reveal based on code drop (10/06/2014)

--- app/js/app/honest/answer_reveal.js
+++ app/js/app/honest/answer_reveal.js
@@ -6,15 +6,37 @@ define(['jquery'],
     {  
         $(function()
         {
+            var hide = function(item)
+            {
+                $(item).next("div").hide();
+                $(item).removeClass('revealed');
+                $(item).text("Show Answer");
+            };
+            
+            var show = function(item)
+            {
+                $(item).next("div").show();
+                $(item).addClass('revealed');
+                $(item).text("Hide Answer");
+            };
+            
            // If 'show answer' clicked - reveal answer
            $(".ru_answer_reveal div:first-child").click(function()
            {
-               $(this).next("div").show();
+               if($(this).hasClass('revealed'))
+               {
+                   hide(this);
+               }
+               else
+               {
+                   show(this);
+               }
+               
            });
            // If close clicked - hide answer
            $(".ru_answer_close").click(function()
            {
-               $(this).parent().hide();
+               hide($(this).parent().parent().find(".revealed"));
            });
         });
     }
