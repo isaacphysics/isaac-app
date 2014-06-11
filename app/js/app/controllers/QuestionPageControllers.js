e999fe2c845787c8a37354efb9c14b88c2e974f9
Stephen Cummins <sacummins@gmail.com>
2014-06-11 14:44:33 +0100

    Hooked up level to question header

--- app/js/app/controllers/QuestionPageControllers.js
+++ app/js/app/controllers/QuestionPageControllers.js
@@ -3,6 +3,7 @@ define([], function() {
 	var HeaderController = ['$scope', 'page', function($scope, page) {
 		$scope.title = page.contentObject.title;
 		$scope.subtitle = page.contentObject.subtitle;
+		$scope.level = page.contentObject.level;
 	}];
 
 	var BodyController = ['$scope', 'page', function($scope, page) {
