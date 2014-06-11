f282c18254112fc24d689d3c231f406801f29430
Stephen Cummins <sacummins@gmail.com>
2014-06-11 16:11:06 +0100

    Added quick questions and set this as the default directive for Symbolic and Numeric questions. The javascript will need to be angularfied using ng-show instead of jquery in the directive js file.

--- app/js/app/directives.js
+++ app/js/app/directives.js
@@ -14,7 +14,8 @@ define([
 	"app/directives/content/Tabs",
 	"app/directives/content/Accordion",
 	"app/directives/content/FeaturedProfile",
-	"app/directives/content/MultiChoiceQuestion"
+	"app/directives/content/MultiChoiceQuestion",
+	"app/directives/content/QuickQuestion"
 	], function() {
 
 	/* Directives */
@@ -50,5 +51,7 @@ define([
 	.directive('isaacFeaturedProfile', require("app/directives/content/FeaturedProfile"))	
 
 	.directive('isaacMultiChoiceQuestion', require("app/directives/content/MultiChoiceQuestion"))
+
+	.directive('isaacQuickQuestion', require("app/directives/content/QuickQuestion"))
 
 });
