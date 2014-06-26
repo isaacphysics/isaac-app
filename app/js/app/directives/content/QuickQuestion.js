1184a57dbb71af94c9b5540b891ef978e201d3e4
Stephen Cummins <sacummins@gmail.com>
2014-06-26 14:34:41 +0100

    Had a go at angularifying the show hide quick questions.

--- app/js/app/directives/content/QuickQuestion.js
+++ app/js/app/directives/content/QuickQuestion.js
@@ -13,21 +13,10 @@ define([], function() {
 			templateUrl: "/partials/content/QuickQuestion.html",
 
 			link: function(scope, element, attrs) {
-		       
-		       // extracted from honest/answer_reveal.js - code drop prior to 10/06/2014
-		       var showHide = $(function()
-		        {
-		           // If 'show answer' clicked - reveal answer
-		           $(".ru_answer_reveal div:first-child").click(function()
-		           {
-		               $(this).next("div").show();
-		           });
-		           // If close clicked - hide answer
-		           $(".ru_answer_close").click(function()
-		           {
-		               $(this).parent().hide();
-		           });
-		        });	
+		       scope.isVisible = false;
+		       scope.toggleVisibility = function(){
+		       		scope.isVisible = !scope.isVisible;
+		       }
 			}
 		};
 	}];
