isaacphysics/isaac-app

commit 866f02ac06d3d1202928b4f6d41174e6050c4728

Redesign content directives to use inherited scopes.

app/js/app/directives/content/Content.js
@@ -5,17 +5,20 @@ define([], function() {
 
 		return {
 
-			scope: {
-				doc: "=",
-			},
+			scope: true,
 
-			restrict: 'EA',
+			restrict: 'A',
 
 			templateUrl: '/partials/content/Content.html',
 
 			compile: function(element) {
 	            return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
 	            	// Post-link actions go here.
+
+	            	scope.doc = undefined;
+	            	scope.$parent.$watch(iAttrs.doc, function(newDoc) {
+	            		scope.doc = newDoc;
+	            	});
 
 	            	if(scope.doc && scope.doc.layout=="right") {
 	            		iElement.css("float", "right").width(300);

app/js/app/directives/content/Accordion.js
@@ -5,18 +5,31 @@ define([], function() {
 
 		return {
 
-			scope: {
-				doc: "=",
-			},
+			scope: true,
 
-			restrict: 'EA',
+			restrict: 'A',
 
 			templateUrl: "/partials/content/Accordion.html",
 
 			link: function(scope, element, attrs) {
 
+				scope.$parent.$watch(attrs.doc, function(newDoc) {
+					scope.doc = newDoc;
+				});
+				
+				// Work out whether we're on a question page. If we are, open the first accordion section. Otherwise, only open it if it is the first item on the page.
+				var isOnQuestionPage = false;
+				var p = scope;
+				while(p = p.$parent) {
+					if (!p.doc)
+						continue;
+
+					if (p.doc.type == "isaacQuestionPage")
+						isOnQuestionPage = true;
+				}
+
 				scope.openChildren = {
-					0: true
+					0: scope.contentChunkIndex == 0 || isOnQuestionPage,
 				};
 
 				scope.toggleChild = function(i) {

app/js/app/directives/content/FeaturedProfile.js
@@ -5,18 +5,22 @@ define([], function() {
 
 		return {
 
-			scope: {
-				doc: "=isaacFeaturedProfile",
-			},
+			scope: true,
 
-			restrict: 'EA',
+			restrict: 'A',
 
 			templateUrl: "/partials/content/FeaturedProfile.html",
 
 			link: function(scope, element, attrs) {
-				if(scope.doc.image){
-					scope.doc.image.src = api.getImageUrl(scope.doc.image.src);	
-				}
+
+				scope.doc = undefined;
+				scope.$parent.$watch(attrs.isaacFeaturedProfile, function(newDoc) {
+					scope.doc = newDoc;
+
+					if(scope.doc.image){
+						scope.doc.image.src = api.getImageUrl(scope.doc.image.src);	
+					}
+				});
 			}
 		};
 	}];

app/js/app/directives/content/Figure.js
@@ -5,17 +5,23 @@ define([], function() {
 
 		return {
 
-			scope: {
-				doc: "=isaacFigure",
-			},
+			scope: true,
 
 			restrict: 'A',
 
 			templateUrl: "/partials/content/Figure.html",
 
 			link: function(scope, element, attrs) {
-				var src = api.getImageUrl(scope.doc.src);
-				scope.imgSrc = src;
+
+				scope.doc = undefined;
+				scope.imgSrc = undefined;
+				
+				scope.$parent.$watch(attrs.isaacFigure, function(newDoc) {
+					scope.doc = newDoc;
+
+					var src = api.getImageUrl(scope.doc.src);
+					scope.imgSrc = src;
+				});
 
 			}
 		};

app/js/app/directives/content/Image.js
@@ -4,18 +4,22 @@ define([], function() {
 	return ["api", function(api) {
 
 		return {
-			scope: {
-				src: "=",
-				isaacImage: "=",
-			},
+			scope: true,
 
-			restrict: 'EA',
+			restrict: 'A',
 
 			templateUrl: "/partials/content/Image.html",
 
 			link: function(scope, element, attrs) {
-				var src = scope.isaacImage || scope.src;
-				scope.path = api.getImageUrl(src);
+
+				scope.src = undefined;
+				scope.path = undefined;
+				scope.$parent.$watch(attrs.isaacImage, function(newSrc) {
+					scope.src = newSrc;
+
+					scope.path = api.getImageUrl(scope.src);
+				});
+
 			}
 		};
 	}];

app/js/app/directives/content/MultiChoiceQuestion.js
@@ -4,15 +4,18 @@ define(["app/honest/responsive_video"], function(rv) {
 	return ["api", function(api) {
 
 		return {
-			scope: {
-				doc: "=isaacMultiChoiceQuestion"
-			},
+			scope: true,
 
-			restrict: 'EA',
+			restrict: 'A',
 
 			templateUrl: "/partials/content/MultiChoiceQuestion.html",
 
 			link: function(scope, element, attrs) {
+
+				scope.doc = undefined;
+				scope.$parent.$watch(attrs.isaacMultiChoiceQuestion, function(newDoc) {
+					scope.doc = newDoc;
+				});
 
 				scope.$watch("selectedAnswer", function() {
 					scope.selectedChoice = scope.doc.choices[scope.selectedAnswer];

app/js/app/directives/content/NumericQuestion.js
@@ -4,15 +4,18 @@ define(["app/honest/responsive_video"], function(rv) {
 	return ["api", function(api) {
 
 		return {
-			scope: {
-				doc: "=isaacNumericQuestion"
-			},
+			scope: true,
 
-			restrict: 'EA',
+			restrict: 'A',
 
 			templateUrl: "/partials/content/NumericQuestion.html",
 
 			link: function(scope, element, attrs) {
+
+				scope.doc = undefined;
+				scope.$parent.$watch(attrs.isaacNumericQuestion, function(newDoc) {
+					scope.doc = newDoc;
+				});
 
 				scope.selectedChoice = {
 					type: "quantity",

app/js/app/directives/content/PageFragment.js
@@ -12,6 +12,7 @@ define([], function() {
 			template: '<isaac-content doc="content" />',
 
 			link: function(scope, element, attrs) {
+
 				var id = attrs.isaacPageFragment;
 
 				api.pageFragments.get({id: id}).$promise.then(function(c) {

commit 07e70d649eb5e129c1c7e7e488b9935584c40ab2

Fix bug introduced in 866f02ac06d3d1202928b4f6d41174e6050c4728.

app/js/app/directives/content/Content.js
@@ -18,11 +18,12 @@ define([], function() {
 	            	scope.doc = undefined;
 	            	scope.$parent.$watch(iAttrs.doc, function(newDoc) {
 	            		scope.doc = newDoc;
+	            		
+		            	if(scope.doc && scope.doc.layout=="right") {
+		            		iElement.css("float", "right").width(300);
+		            	}
 	            	});
 
-	            	if(scope.doc && scope.doc.layout=="right") {
-	            		iElement.css("float", "right").width(300);
-	            	}
 	            });
 	        }
 		};