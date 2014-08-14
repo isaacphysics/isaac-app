7323fdccbd0c861dcd9689a884d63bae5caae4ed
Luke McLean <luke@honestideas.co.uk>
2014-08-14 15:34:24 +0100

    Can now login but some error handling is required.

--- app/js/app/controllers/LoginPageControllers.js
+++ app/js/app/controllers/LoginPageControllers.js
@@ -1,10 +1,15 @@
 define([], function() {
 
-	var PageController = ['$scope', 'auth', '$stateParams', function($scope, auth, $stateParams) {
+	var PageController = ['$scope', 'auth', 'api', '$stateParams', '$location', function($scope, auth, api, $stateParams, $location) {
 
 		$scope.auth = auth;
-		$scope.target = $stateParams.target;
+		$scope.target = $stateParams.target; 
 		$scope.globalFlags.noSearch = true;
+
+		$scope.login = function() {
+			api.loginEndpoint.login($scope.user);
+			//$location.path('/');
+		}
 		
 	}]
 
