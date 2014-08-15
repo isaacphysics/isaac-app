44e60d9e2839756ce0291421bdd718633b24830f
Luke McLean <luke@honestideas.co.uk>
2014-08-15 12:35:10 +0100

    dob selects added

--- app/js/app/controllers/AccountSettingsPageControllers.js
+++ app/js/app/controllers/AccountSettingsPageControllers.js
@@ -9,6 +9,32 @@ define([], function() {
 		}
 
 		$scope.globalFlags.noSearch = true;
+
+		$scope.dob = {
+			days: function(){
+				var i =1,
+					days = [];
+
+				while(i <= 31){
+					days.push(i++);
+				}
+				return days;
+			},
+			months: function(){
+				var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+				return months;
+			},
+			years: function(){
+				var currentYear = new Date().getFullYear(),
+				years = [],
+				startYear = 1900;
+
+				while(startYear <= currentYear){
+					years.push(startYear++);
+				}
+				return years.reverse();
+			}
+		}
 		
 	}]
 
