bd78a1bd9a407191323772051aed78f9c1a6bb4c
Nick Rogers <rogersnm@hotmail.co.uk>
2014-08-19 15:18:17 +0100

    Fixes #164

--- app/js/app/controllers/AccountSettingsPageControllers.js
+++ app/js/app/controllers/AccountSettingsPageControllers.js
@@ -1,50 +1,53 @@
 define([], function() {
 
 	var PageController = ['$scope', 'auth', 'api', '$stateParams', function($scope, auth, api, $stateParams) {
-		// Grab these for setting date later
-		var dobDay = document.getElementById('dob-day'),
-			dobMonth = document.getElementById('dob-month'),
-			dobYear = document.getElementById('dob-year');
+		// Create date of birth select options
+		$scope.datePicker = {
+			days: [],
+			months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			years: []
+		};
 
-		$scope.user = auth.getUser();
+		// Populate days
+		for (var i = 1; i <= 31; i++) {
+			$scope.datePicker.days.push(i);
+		}
+
+		// Populate years
+		var currentYear = new Date().getFullYear();
+		for (var i = currentYear; i > 1900; i--) {
+			$scope.datePicker.years.push(i);
+		}
+
+		$scope.dob = {};
+
+		auth.getUser().$promise.then(function(user) {
+			$scope.user = user;
+			if ($scope.user.dateOfBirth != null) {
+				var date = new Date($scope.user.dateOfBirth);
+				$scope.dob.day = date.getDate();
+				$scope.dob.month = $scope.datePicker.months[date.getMonth()];
+				$scope.dob.year = date.getFullYear();
+			}
+		});
+
+		// Watch for changes to the DOB selection
+		$scope.$watchCollection('dob', function() {
+			if ($scope.user == null) {
+				// User object hasn't been initialised yet
+				return;
+			}
+
+			var dob = new Date($scope.dob.year, $scope.datePicker.months.indexOf($scope.dob.month), $scope.dob.day);
+			if (!isNaN(dob.getTime())) {
+				$scope.user.dateOfBirth = dob.getTime();
+			}
+		});
 
 		// Remove search
 		$scope.globalFlags.noSearch = true;
 
-		// Create date of birth select options
-		$scope.dob = {
-			days: function(){
-				var i =1,
-					days = [];
-
-				while(i <= 31){
-					days.push(i++);
-				}
-				return days;
-			},
-			months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-			years: function(){
-				var currentYear = new Date().getFullYear(),
-					years = [],
-					startYear = 1900;
-
-				while(startYear <= currentYear){
-					years.push(startYear++);
-				}
-				return years.reverse();
-			}
-		}
 		$scope.save = function() {
-			var dobDayValue = dobDay.options[dobDay.selectedIndex].value,
-				dobMonthValue = ++dobMonth.options[dobMonth.selectedIndex].value,
-				dobYearValue = dobYear.options[dobYear.selectedIndex].value,
-				// Check date is valid
-				valid = new Date(dobYearValue, dobMonthValue, dobDayValue).getMonth() == dobMonthValue;
-
-			alert('Date valid = '+valid +' ('+dobDayValue+dobMonthValue+dobYearValue+')');
-
-			// Set dob before posting to the API
-			$scope.user.dateOfBirth = dobDayValue+dobMonthValue+dobYearValue;
 			api.account.saveSettings($scope.user);
 		}
 
