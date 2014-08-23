0fabbe116343c26fc49800afb43ca82599a5e7ed
Nick Rogers <rogersnm@hotmail.co.uk>
2014-08-23 21:08:30 +0100

    Fixes #202

--- app/js/app/controllers/MyBoardsPageControllers.js
+++ app/js/app/controllers/MyBoardsPageControllers.js
@@ -1,6 +1,7 @@
 define([], function() {
 
 	var PageController = ['$scope', 'auth', 'api', 'gameBoardTitles', function($scope, auth, api, gameBoardTitles) {
+		$scope.globalFlags.isLoading = true;
 		$scope.user = auth.getUser();
 
 		$scope.generateGameBoardTitle = gameBoardTitles.generate;
@@ -9,29 +10,53 @@ define([], function() {
 			{label: "All Boards", val: null},
 			{label: "Not Started", val: "not_attempted"},
 			{label: "In Progress", val: "in_progress"},
-			{label: "Completed", val: "completed"},
+			{label: "Completed", val: "completed"}
 		];
 
 		$scope.sortOptions = [
 			{label: "Date Created", val: "created"},
 			{label: "Date Visited", val: "visited"},
-			{label: "Subject", val: "subject"},
+			{label: "Subject", val: "subject"}
 		];
 
 		$scope.filterOption = $scope.filterOptions[0];
 		$scope.sortOption = $scope.sortOptions[0];
 
-
 		var updateBoards = function() {
-			$scope.boards = api.userGameBoards($scope.filterOption.val, $scope.sortOption.val, 0);
-		}
+			$scope.globalFlags.isLoading = true;
+			api.userGameBoards($scope.filterOption.val, $scope.sortOption.val, 0).$promise.then(function(boards) {
+				$scope.boards = boards;
+				$scope.globalFlags.isLoading = false;
+				$scope.globalFlags.displayLoadingMessage = false;
+			})
+		};
+
+		// update boards when filters have been selected
+		$scope.$watch("filterOption", function(newVal, oldVal) {
+			// TODO: For some reason these watch functions are being fired for no reason
+			if (newVal === oldVal) {
+				return;
+			}
+			updateBoards();
+		});
+
+		$scope.$watch("sortOption", function(newVal, oldVal) {
+			if (newVal === oldVal) {
+				return;
+			}
+			updateBoards();
+		});
+
+		// Perform initial load
+		updateBoards();
 
 		$scope.loadMore = function() {
 			api.userGameBoards($scope.filterOption.val, $scope.sortOption.val, $scope.boards.results.length).$promise.then(function(newBoards){
 				// Merge new boards into results 
 				$.merge($scope.boards.results, newBoards.results);
 			});
-		}
+		};
+
 		$scope.deleteBoard = function(id, name){
 			// Warn user before deleting
 			var confirmation = confirm("You are about to delete "+name+" board?");
@@ -46,13 +71,9 @@ define([], function() {
        			});
 			}
 		}
-
-		// update boards when filters have been selected
-		$scope.$watch("filterOption", updateBoards);
-		$scope.$watch("sortOption", updateBoards);
-	}]
+	}];
 
 	return {
-		PageController: PageController,
+		PageController: PageController
 	};
 })
