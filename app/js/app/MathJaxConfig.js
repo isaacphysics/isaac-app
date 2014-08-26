14c35d92179351c726711b448139598dca0beab6
Ian Davies <git@iandavies.org>
2014-08-26 16:43:48 +0100

    Reduce space after "d" operator. Fixes #228.

--- app/js/app/MathJaxConfig.js
+++ app/js/app/MathJaxConfig.js
@@ -24,7 +24,7 @@ define(["mathjax"], function() {
 				"quantity": ["{#1}\\,{\\rm{#2}}",2],
 				"valuedef": ["{#1}={\\quantity{#2}{#3}}",3],
 				"vtr": ["{\\underline{\\boldsymbol{#1}}}",1],
-				"d": "\\operatorname{d}",
+				"d": "\\mathrm{d}\\,",
 				"vari": ["#1",1],
 				"s": ["_{\\sf{#1}}", 1],
 				"half": ["\\frac{1}{2}",0],
