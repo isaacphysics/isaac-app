6216f30ac4d2366c3e2138841eae84b6a1b9ee60
Stephen Cummins <sacummins@gmail.com>
2014-09-29 15:22:28 +0100

    Removed unnecessary code.

--- app/js/app/controllers/LoginPageControllers.js
+++ app/js/app/controllers/LoginPageControllers.js
@@ -20,12 +20,7 @@ define([], function() {
 		$scope.globalFlags.noSearch = true;
 		
 		// Make sure the internal user object is up-to-date
-		auth.updateUser().then(function(user){
-			if (!user) {
-				// if we don't currently have a user then this is fine we can show them the login page.
-				return;
-			} 
-			
+		auth.updateUser().then(function(user){			
 			// We will only do this bit if the user is already logged in.
 			var target = $location.search().target;
 			if (target) {
