27f227b9f9446ce970a8853465dc68a5e3a8f053
Ian Davies <git@iandavies.org>
2014-11-09 19:12:21 +0000

    Functional (!) top menus.

--- app/js/app/directives.js
+++ app/js/app/directives.js
@@ -232,4 +232,22 @@ define([
 			}
 		}
 	}])
+
+	.directive("ngScopeElement", [function () {
+	  var directiveDefinitionObject = {
+
+	    restrict: "A",
+
+	    compile: function compile(tElement, tAttrs, transclude) {
+	      return {
+	          pre: function preLink(scope, iElement, iAttrs, controller) {
+	            scope[iAttrs.ngScopeElement] = iElement;
+	          }
+	        };
+	    }
+	  };
+
+	  return directiveDefinitionObject;
+	}])
+	
 });
--- app/js/app/directives/equation_editor/EquationEditor.js
+++ app/js/app/directives/equation_editor/EquationEditor.js
@@ -1,12 +1,32 @@
 define([], function() {
 
-	return [function() {
+	return ["$timeout", function($timeout) {
 
 		return {
 			restrict: "A",
 			templateUrl: "/partials/equation_editor/equation_editor.html",
 			link: function(scope, element, attrs) {
 
+                element.find(".top-menu").css("bottom", element.height());
+
+                scope.clickHandle = function(which) {
+
+                    if (scope.menuActive == which) {
+                        scope.menuActive = null;
+                        element.find(".top-menu").animate({"bottom": element.height()}, 200);
+
+                    } else {
+                        scope.menuActive = which;
+                        element.find(".top-menu.active").removeClass("active");
+                        scope[which].addClass("active");
+                        var activeMenuHeight = scope[which].height();
+                        element.find(".top-menu").animate({"bottom": element.height() - activeMenuHeight}, 200);
+
+                    }
+                };
+
+
+ 
 			},
 
 		};
