1d62904480b4a8eeeb3cfdf11f944de493f1006d
Ian Davies <git@iandavies.org>
2015-01-26 16:02:23 +0000

    Spell array correctly...

--- app/js/app/directives/PodCarousel.js
+++ app/js/app/directives/PodCarousel.js
@@ -29,7 +29,7 @@ define([], function() {
 				scope.pods = api.pods.get();
 
 				scope.pods.$promise.then(function(){
-					array.prototype.sort.call(scope.pods,function(a,b) { return a.id > b.id; });
+					Array.prototype.sort.call(scope.pods,function(a,b) { return a.id > b.id; });
 				});
 
 				$timeout(function() {
