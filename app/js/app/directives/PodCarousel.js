29f9b000de2f6bdecb6657185286c48a778fc876
Stephen Cummins <sacummins@gmail.com>
2015-01-28 16:14:31 +0000

    Changed carousel approach as there seemed to be some performance problems (i.e. it was requesting the images from the server each time the pod loaded even if it had been seen before)

--- app/js/app/directives/PodCarousel.js
+++ app/js/app/directives/PodCarousel.js
@@ -13,28 +13,93 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-define([], function() {
+ define([], function() {
 
-	return ["api", "$timeout", function(api, $timeout) {
+ 	return ["api", "$interval", function(api, $interval) {
 
-		return {
-			scope: true,
+ 		return {
+ 			scope: true,
 
-			restrict: "A",
+ 			restrict: "A",
 
-			templateUrl: "/partials/pod_carousel.html",
+ 			templateUrl: "/partials/pod_carousel.html",
 
-			link: function(scope, element, attrs) {
-				scope.startPod = 0;
-				scope.pods = api.pods.get();
+ 			link: function(scope, element, attrs) {
+ 				var intervalReference = null;
+ 				var intervalValue = 10000; //ms
+ 				
+ 				scope.startPod = 0;
+ 				scope.pods = api.pods.get();
+
+ 				scope.visiblePods = [];
+
+ 				var calculateVisiblePods = function() {
+ 					scope.visiblePods = [
+	 					scope.pods.results[scope.startPod % scope.pods.results.length],
+	 					scope.pods.results[(scope.startPod + 1) % scope.pods.results.length],
+	 					scope.pods.results[(scope.startPod + 2) % scope.pods.results.length]
+ 					]
+ 				}
+
+				var interruptAndSetupNewInterval = function() {
+					if (intervalReference != null) {
+						$interval.cancel(intervalReference);
+						interval = null;
+					}
+
+					intervalReference = $interval(function() {
+						scope.startPod++;
+					}, 10000);
+				}
 
 				scope.pods.$promise.then(function(){
 					Array.prototype.sort.call(scope.pods.results,function(a,b) { return a.id > b.id; });
+					calculateVisiblePods();
+					interruptAndSetupNewInterval();
 				});
 
-				$timeout(function() {
-					scope.startPod++;
-				}, 10000);
+				scope.pauseCarousel = function() {
+					if (intervalReference != null) {
+						$interval.cancel(intervalReference);
+						intervalReference = null;
+					}
+				}
+
+				scope.startCarousel = interruptAndSetupNewInterval;
+				
+				/**
+				 * Function to navigate through the pods available.
+				 */
+				scope.navigate = function(positionAdjustment) {
+					scope.startPod = scope.startPod + positionAdjustment;
+					interruptAndSetupNewInterval();
+				}
+
+				/**
+				 * Function to determine whether a given pod should be visible or not.
+				 * This assumes desktop resolution.
+				 */
+				scope.isVisible = function(pod) {
+					calculateVisiblePods();
+
+					if (scope.visiblePods.indexOf(pod) != -1 ) {
+						return true;
+					}
+					
+					return false;
+				}
+
+				/**
+				 * Function to determine the order of pods to be displayed.
+				 */
+				scope.comparitor = function(pod) {
+					var pos = scope.visiblePods.indexOf(pod);
+					if(pos != -1) {
+						return pos;
+					} 
+
+					return scope.pods.results.length;
+				}
 			},
 		};
 	}];
