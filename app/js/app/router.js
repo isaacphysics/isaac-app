efe3c69bf23ddf29e388355560c24db7354c11b1
Ian Davies <git@iandavies.org>
2015-04-28 11:33:58 +0100

    Hide equation editor behind admin login.

--- app/js/app/router.js
+++ app/js/app/router.js
@@ -107,7 +107,18 @@ define(["angular-ui-router"], function() {
             .state('physics_skills_14', genericPageState("/physics_skills_14", "physics_skills_14_index"))
             .state('top_boards', genericPageState("/top_boards", "pop_boards"))
             .state('publications', genericPageState("/publications", "publications"))
-            .state('equality', staticPageState("/equality", "equation_editor", null))
+
+            .state('equality', {
+                url: "/equality",
+                resolve: {
+                    requireRole: getRolePromiseInjectableFunction(["ADMIN"]),
+                },
+                views: {
+                    "body": { 
+                        templateUrl: "/partials/states/equation_editor.html",
+                    },
+                },
+            })
 
 	        .state('contact', {
 		        url: "/contact",
