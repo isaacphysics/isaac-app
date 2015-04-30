72741ca820d72e0a2405d2c834cf1d21d363453c
Luke McLean <luke@honestideas.co.uk>
2015-04-30 15:08:06 +0100

    Bar graph updated, key still to be added.

--- app/js/app/directives/d3/Bar.js
+++ app/js/app/directives/d3/Bar.js
@@ -26,29 +26,29 @@ define([], function() {
                     
                 scope.$watch('data', function(){
 
-                    var w = 960,
+                    var w = 500,
                         h = 500
 
                     // create canvas
                     var svg = d3.select(element[0])
                         .append("svg")
-                        .attr("class", "chart")
+                        .attr("class", "d3-bar")
                         .attr("width", '100%')
                         .attr("height", '100%')
                         .attr('viewBox','0 0 '+w+' '+h)
                         .attr('preserveAspectRatio','xMinYMin')
                         .append("svg:g")
-                        .attr("transform", "translate(10,470)");
+                        .attr("transform", "translate(50, 460)");
 
                     x = d3.scale.ordinal().rangeRoundBands([0, w-50])
                     y = d3.scale.linear().range([0, h-50])
                     z = d3.scale.ordinal().range(['#4fa446', '#7dc571', '#bbdda9']);
 
                     var matrix = [
-                        [ 1,  5871, 8916, 2868],
-                        [ 2, 10048, 2060, 6171],
-                        [ 3, 16145, 8090, 8045],
-                        [ 4,   990,  940, 6907]
+                        [ 1, 2, 4, 2.5],
+                        [ 2, 8, 1, 0],
+                        [ 3, 3, 6, 8],
+                        [ 4, 1,  3, 1]
                     ];
 
                     var remapped =["c1","c2","c3"].map(function(dat,i){
@@ -59,7 +59,6 @@ define([], function() {
                     console.log(remapped)
 
                     var stacked = d3.layout.stack()(remapped)
-                    console.log(stacked)
 
                     x.domain(stacked[0].map(function(d) { return d.x; }));
                     y.domain([0, d3.max(stacked[stacked.length - 1], function(d) { return d.y0 + d.y; })]);
@@ -68,7 +67,7 @@ define([], function() {
                     // Add a group for each column.
                     var valgroup = svg.selectAll("g.valgroup")
                         .data(stacked)
-                        .enter().append("svg:g")
+                        .enter().append("g")
                         .attr("class", "valgroup")
                         .style("fill", function(d, i) { return z(i); })
                         .style("stroke", function(d, i) { return d3.rgb(z(i)).darker(); });
@@ -81,6 +80,30 @@ define([], function() {
                         .attr("y", function(d) { return -y(d.y0) - y(d.y); })
                         .attr("height", function(d) { return y(d.y); })
                         .attr("width", x.rangeBand());
+
+
+                    var xAxis = d3.svg.axis()
+                        .scale(x)
+                        .orient("bottom");
+
+                    var yAxis = d3.svg.axis()
+                        .scale(y)
+                        .orient("left");
+       
+
+                    svg.append("g")
+                        .attr("class", "x-axis")
+                        .attr("transform", "translate(0, 10)")
+                        .call(xAxis);
+
+                    svg.append("g")
+                        .attr("class", "y-axis")
+                        .attr("transform", "translate(-20, -450)")
+                        .call(yAxis);
+
+                    // add next and previous
+                    d3.select(element[0]).append("div").attr("class", "d3-bar-prev") 
+                    d3.select(element[0]).append("div").attr("class", "d3-bar-next");
 
                 });
             
