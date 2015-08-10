aa7473ae36b16aa2b5092f6410a618e8a1ce836c
Stephen Cummins <sacummins@gmail.com>
2015-08-10 12:17:25 +0100

    Add cookie remove function

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -368,6 +368,10 @@ define([
                             }
                         }
                         return null;
+                    },
+                    remove: function(name) {
+                        // make it expire
+                        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
                     }
                 }
 
