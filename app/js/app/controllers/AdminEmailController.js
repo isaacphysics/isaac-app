a314d9a83e0ad755296e3a697f8c1a217a0f7e2f
Alistair Stead <alistair.stead@gmail.com>
2015-11-16 17:03:47 +0000

    Email previews now work. Admin email validation not so much.

--- app/js/app/controllers/AdminEmailController.js
+++ app/js/app/controllers/AdminEmailController.js
@@ -15,34 +15,79 @@
  */
 define([], function() {
 
-	var PageController = ['$scope', 'auth', 'api', '$window', '$rootScope', function($scope, auth, api, $window, $rootScope) {
+	var PageController = ['$scope', 'auth', 'api', '$window', '$rootScope', '$sce', function($scope, auth, api, $window, $rootScope, $sce) {
 		$scope.isAdminUser = $rootScope.user.role == 'ADMIN';
 	    $scope.statistics = null;
 	    $scope.setLoading(true);
 	    $scope.apiVersion = api.contentVersion.get();
+	    $scope.emailTypes = null;
 
-	    $scope.emailConsoleProperties = {
-	    	adminUsers : false,
-	    	eventManagerUsers : false,
-	    	contentEditorUsers : false,
-	    	teacherUsers : false,
-	    	testerUsers : false,
-	    	studentUsers : false,
-	    	contentObjectId : ""
-	    }
+	    $scope.lastContentIDSuccessfullyPreviewed = "";
+
+	    $scope.htmlPreview = "";
+
+	    $scope.emailToSend = {
+	    	emailType : {"preferenceIndex" : -1},
+	    	contentObject :{"id": ""},
+	    	users: {
+		    	adminUsers : false,
+		    	eventManagerUsers : false,
+		    	contentEditorUsers : false,
+		    	teacherUsers : false,
+		    	testerUsers : false,
+		    	studentUsers : false,
+	    	}
+	    };
 
 	    api.statisticsEndpoint.get().$promise.then(function(result){
 	        $scope.statistics = result;
 	        $scope.setLoading(false);
-	        console.log($scope.statistics);
+	    }).catch(function(e){
+			$scope.showToast($scope.toastTypes.Failure, "Statistics load failed", "With error message (" + e.status + ") " + e.statusText);
+	    });
+
+	    api.email.getPreferences().$promise.then(function(result){
+	    	$scope.emailTypes = result;
+	    }).catch(function(e){
+			$scope.showToast($scope.toastTypes.Failure, "Preferences load failed", "With error message (" + e.status + ") " + e.statusText);
 	    });
 
 	    $scope.loadContentTemplate = function(contentId){
 	    	$scope.setLoading(true);
-	    	api.emailTemplate.get($scope.emailConsoleProperties.contentObjectId).$promise.then(function(content){
-	    		console.log(content);
+	    	api.email.get({"id" : $scope.emailToSend.contentObjectId}).$promise.then(function(content){
+	        	$scope.htmlPreview = $sce.trustAsResourceUrl("data:text/html," + encodeURIComponent(content.html));
 	        	$scope.setLoading(false);
+	        	$scope.lastContentIDSuccessfullyPreviewed = $scope.emailToSend.contentObjectId;
+	    	}).catch(function(e){
+    			$scope.showToast($scope.toastTypes.Failure, "Email template failed", "With error message (" + e.status + ") " + e.statusText);
+	        	$scope.setLoading(false);
+	        	$scope.lastContentIDSuccessfullyPreviewed = "";
 	    	});
+	    }
+
+	    $scope.validateAndSendEmails = function(){
+	    	//TODO get the email preference out of the dropdown
+	    	console.log($scope.emailTypes);
+
+	    	$scope.emailToSend.emailType.preferenceIndex = $scope.emailTypes;
+	    	if($scope.emailToSend.emailType < 0){
+	    		$scope.emailToSend.emailType.$invalid = true;
+	    	}
+
+	    	if(!$scope.contentLoaded || !$scope.contentLoaded.id || !$scope.contentLoaded.id == "" || $scope.contentLoaded != $scope.emailToSend.contentObjectId){
+				$scope.emailToSend.contentObjectId.$invalid = true;
+	    	}
+
+	    	$scope.emailToSend.users.$invalid = true;
+	    	for(var key in $scope.emailToSend.users) {
+	    		if(key.substring(0, 1) != "$"){
+	    			if($scope.emailToSend.users[key]){
+	    				$scope.emailToSend.users.$invalid = false;
+	    			}
+	    		}
+	    	}
+
+
 	    }
 
     }]
