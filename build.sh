007ec94e81b329c90ab9946308c85a6d02a9bb81
Ian Davies <git@iandavies.org>
2016-04-21 09:44:35 +0100

    Try using a git SHA as API version.

--- build.sh
+++ build.sh
@@ -19,20 +19,15 @@ cd $BUILD_DIR
 git clone -b $VERSION_TO_DEPLOY --depth 1 https://github.com/ucam-cl-dtg/isaac-app.git
 cd isaac-app
 
-# Determine segue version to use. Honest.
-if [[ $VERSION_TO_DEPLOY == v* ]]; then
-	SEGUE_VERSION=`grep urlPrefix\(\"/api/ < app/js/app/app.js | sed 's/.*\/api\/\([^\/]*\)\/api.*/\1/g'`
+if test -z "$2"; then
+    # If there is no second arg, use SEGUE VERSION from the app src
+    SEGUE_VERSION=`grep urlPrefix\(\"/api/ < app/js/app/app.js | sed 's/.*\/api\/\([^\/]*\)\/api.*/\1/g'`
 else
-	# Change the app src to request the API from a particular branch
-	if test -z "$2"; then
-          read -p "Override API version to target [$VERSION_TO_DEPLOY]" SEGUE_VERSION
-	else
-          SEGUE_VERSION=$2
-        fi
-        SEGUE_VERSION=${SEGUE_VERSION:-$VERSION_TO_DEPLOY}
+    # Otherwise, override the SEGUE_VERSION in the app src to use the specified ref.
+    SEGUE_VERSION=$2
 
-	sed -i.bak "s/api\/[^\/]*\/api/api\/$SEGUE_VERSION\/api/g" app/js/app/app.js
-	rm app/js/app/app.js.bak
+    sed -i.bak "s/api\/[^\/]*\/api/api\/$SEGUE_VERSION\/api/g" app/js/app/app.js
+    rm app/js/app/app.js.bak
 fi
 
 npm install
