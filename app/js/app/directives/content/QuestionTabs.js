b3c138dd7e728998b34106b151f5376886204a5a
James Sharkey <jsharkey13@users.noreply.github.com>
2016-04-29 17:32:55 +0100

    Avoid rage clicking by ensuring answer has changed before accepting a new one (needs thorough testing), also display toast popup for any server error received!

--- app/js/app/directives/content/QuestionTabs.js
+++ app/js/app/directives/content/QuestionTabs.js
@@ -28,6 +28,8 @@ define(["app/honest/responsive_video"], function(rv) {
 
 			link: function(scope, element, attrs, ctrls, transclude) {
 
+				scope.previousAnswerChoice = null;
+
 				if (scope.accordionChildMetrics) {
 					scope.accordionChildMetrics.questionCount++;
 				}
@@ -67,7 +69,8 @@ define(["app/honest/responsive_video"], function(rv) {
 				}
 
 				scope.checkAnswer = function() {
-					if (scope.selectedChoice != null) {
+					if (scope.selectedChoice != null && scope.selectedChoice != scope.previousAnswerChoice) {
+						scope.previousAnswerChoice = scope.selectedChoice;
 
 						if (scope.doc.type == "isaacSymbolicQuestion") {
 							var symbols = JSON.parse(scope.selectedChoice.value).symbols;
@@ -118,6 +121,7 @@ define(["app/honest/responsive_video"], function(rv) {
 
 						}, function bar(e) {
 							console.error("Error validating answer:", e);
+							scope.showToast(scope.toastTypes.Failure, "Can't Submit Answer", e.data.errorMessage != undefined ? e.data.errorMessage : "");
 						});
 
 					} else {
