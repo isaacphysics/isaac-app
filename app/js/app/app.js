35d2450b73e54ff7114aed180b12863dee7dee26
James Sharkey <jsharkey13@users.noreply.github.com>
2016-06-22 12:52:02 +0100

    Release v1.7.5

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -92,7 +92,7 @@ define([
         if (document.location.hostname == "localhost") {
             apiProvider.urlPrefix("http://localhost:8080/isaac-api/api");
         } else {
-            apiProvider.urlPrefix("/api/v1.7.4/api");
+            apiProvider.urlPrefix("/api/v1.7.5/api");
         }
 
         NProgress.configure({ showSpinner: false });
