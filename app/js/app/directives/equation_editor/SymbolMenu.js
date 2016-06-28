6e8e225e8b93be584458ae2a2ba66545c0bb6aef
Andrea Franceschini <andrea.franceschini@gmail.com>
2016-06-28 14:52:33 +0100

    The symbol menu now hides after dragging on screens of height less than 640px.

--- app/js/app/directives/equation_editor/SymbolMenu.js
+++ app/js/app/directives/equation_editor/SymbolMenu.js
@@ -33,12 +33,12 @@ define([], function() {
 
 					lst.css("left", newLeft);
 
-					if (pageY > element.offset().top + element.height()) {
-						//scope.$emit("triggerCloseMenus");
+					if (pageY > element.offset().top + element.height() && $(window).height() <= 640) {
+						scope.$emit("triggerCloseMenus");
 					}
 
 					scope.$emit("newSymbolDrag", symbol, pageX, pageY, mousePageX, mousePageY);
-				}
+				};
 
 				var abortSymbolDrag = function($e, symbol, pageX, pageY, mousePageX, mousePageY) {
 					bufferedLeft = parseFloat(lst.css("left"));
@@ -49,13 +49,13 @@ define([], function() {
                     } else {
                     	scope.$emit("newSymbolAbortDrag");
                     }
-				}
+				};
 
 				abortSymbolDrag();
 
 				scope.$on("symbolDrag", absorbSymbolDrag);
 				scope.$on("symbolDrop", abortSymbolDrag);
-			},
+			}
 		};
 	}];
 });
