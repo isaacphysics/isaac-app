155d6e9456473184add86963c857b7db50383824
Junwei Yuan <jimyuan1995@outlook.com>
2016-08-09 12:12:27 +0100

    Only works on /questions/graph_sketcher_test

--- app/js/app/directives/graph_sketcher/GraphInput.js
+++ app/js/app/directives/graph_sketcher/GraphInput.js
@@ -8,12 +8,14 @@ define([], function() {
                 questionDoc: "=",
                 
             },
+
             restrict: "A",
             templateUrl: "/partials/graph_sketcher/graph_input.html",
             link: function(scope, element, attrs) {
 
               scope.edit = function() {
                   $rootScope.showGraphSketcher(scope.state, scope.questionDoc, scope.editorMode).then(function(finalState) {
+                      console.debug(finalState);
                       scope.state = finalState;
                       scope.$apply();
                   });
