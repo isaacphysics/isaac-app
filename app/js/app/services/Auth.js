f98a81d6b1db91642ce58e8822ee30449514c123
Ian Davies <git@iandavies.org>
2016-09-05 10:14:04 +0100

    Improve user consistency checking

--- app/js/app/services/Auth.js
+++ app/js/app/services/Auth.js
@@ -90,19 +90,19 @@ define([], function() {
 		}
 
 		this.updateUser = function() {
-			$rootScope.user = api.currentUser.get();
-			
-			$rootScope.user.$promise.then(function(u) {
-				$timeout(function() {
-					$rootScope.user = u;
-					setupUserConsistencyCheck();
-					$rootScope.$apply();
+			return new Promise(function(resolve, reject) {
+				api.currentUser.get().$promise.then(function(u) {
+					$timeout(function() {
+						$rootScope.user = u;
+						setupUserConsistencyCheck();
+						$rootScope.$apply();
+						resolve(u);
+					});
+				}).catch(function(){
+					cancelUserConsistencyCheck();
+					reject();
 				});
-			}).catch(function(){
-				cancelUserConsistencyCheck();
-			})
-
-			return $rootScope.user.$promise;
+			});
 		}
 		
 
