bb9e20e7d95f0be693674c898dca9fa4edaa7e58
James Sharkey <jsharkey13@users.noreply.github.com>
2016-10-31 16:52:19 +0000

    Update iframe_api and polyfill a console to allow old IE support still!

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -65,6 +65,30 @@ define([
         // Send session cookies with the API requests.
         $httpProvider.defaults.withCredentials = true;
 
+        // Polyfill a console for browsers (IE) that don't support one!
+        // https://github.com/h5bp/html5-boilerplate/blob/master/src/js/plugins.js
+        (function() {
+            var method;
+            var noop = function () {};
+            var methods = [
+                'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+                'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+                'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+                'timeStamp', 'trace', 'warn'
+            ];
+            var length = methods.length;
+            var console = (window.console = window.console || {});
+
+            while (length--) {
+                method = methods[length];
+
+                // Only stub undefined methods.
+                if (!console[method]) {
+                    console[method] = noop;
+                }
+            }
+        }());
+
         $httpProvider.interceptors.push(["$q", "$injector", function($q, $injector) {
             return {
                 response: function(response) {
