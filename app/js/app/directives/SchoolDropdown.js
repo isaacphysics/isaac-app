d25a946d109f250160f8ab2f34e42613743a25e1
James Sharkey <jsharkey13@users.noreply.github.com>
2016-11-15 09:58:20 +0000

    Fix a bug with loading no longer recognised school IDs
    # We may want to actually revert to the previous ID before saving, to avoid losing info if they don't update their school again.

--- app/js/app/directives/SchoolDropdown.js
+++ app/js/app/directives/SchoolDropdown.js
@@ -35,8 +35,11 @@ define([], function() {
 			 	scope.$watch("selectedSchoolUrn", function(newUrn, oldUrn) {
 			 		if (newUrn) {
 			 			api.schools.query({query: newUrn}).$promise.then(function(s) {
-				 			if (s.length >= 1)
+				 			if (s.length >= 1) {
 				 				scope.selection.school = s[0];
+			 				} else {
+			 					scope.selectedSchoolUrn = null;
+			 				}
 				 		});
 			 		}
 			 	});
