f5b01f8c188d30cd1be57de06948a948ff109a27
Meurig Thomas <mlt47@cam.ac.uk>
2017-11-14 11:48:48 +0000

    Release v2.1.16

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -118,7 +118,7 @@ define([
             // Have reserved domians on ngrok.io, hardcode them for ease of use:
             apiProvider.urlPrefix("https://isaacscience.eu.ngrok.io/isaac-api/api");
         } else {
-            apiProvider.urlPrefix("/api/v2.2.4/api");
+            apiProvider.urlPrefix("/api/v2.2.5/api");
         }
 
         NProgress.configure({ showSpinner: false });
