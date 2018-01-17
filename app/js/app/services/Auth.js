b8b70d511ecf3fa3b081309a9f5b86ab07ffe032
James Sharkey <jsharkey13@users.noreply.github.com>
2018-01-17 18:03:37 +0000

    Load user preferences onto rootScope at login time
    # Ian helped considerably with this, and whilst it's still awful, the only way
    # to improve it would be a large rewrite that I don't want to do now and break
    # anything by doing.

--- app/js/app/services/Auth.js
+++ app/js/app/services/Auth.js
@@ -88,6 +88,11 @@ define([], function() {
 			return p;
 		}
 
+		var updateUserPreferences = function() {
+			$rootScope.userPreferences = api.user.getUserPreferences();
+			return $rootScope.userPreferences.$promise;
+		}
+
 		this.updateUser = function() {
 			return new Promise(function(resolve, reject) {
 				var userResource = api.currentUser.get();
@@ -96,12 +101,18 @@ define([], function() {
 				}
 				
 				userResource.$promise.then(function(u) {
-					$timeout(function() {
-						$rootScope.user = u;
-						setupUserConsistencyCheck();
-						$rootScope.$apply();
+
+					return new Promise(function(resolve, reject) {
+						$timeout(function() {
+							$rootScope.user = u;
+							setupUserConsistencyCheck();
+							$rootScope.$apply();
+							resolve();
+						});
+					}).then(updateUserPreferences).then(function() {
 						resolve(u);
 					});
+
 				}).catch(function(){
 					cancelUserConsistencyCheck();
 					reject();
