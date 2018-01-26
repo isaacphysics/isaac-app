ac8d3fb9cb15f63152659b688721e89b07962246
Meurig Thomas <mlt47@cam.ac.uk>
2018-01-26 10:01:55 +0000

    Comment correction

--- app/js/app/services/BoardProcessor.js
+++ app/js/app/services/BoardProcessor.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Meurig Thomas
+ * Copyright 2017 Meurig Thomas
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
