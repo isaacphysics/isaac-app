7dba14d972b0109b5e1e07ab2293c7c370e509b7
Meurig Thomas <mlt47@cam.ac.uk>
2018-05-22 13:02:26 +0100

    Don't count events as all day events if the start and end dates are on different days

--- app/js/app/controllers/EventsControllers.js
+++ app/js/app/controllers/EventsControllers.js
@@ -17,7 +17,7 @@ define([], function() {
 
     var augmentEvent = function(e, api) {
         if (e.endDate != null) {  // Non-breaking change; if endDate not specified, behaviour as before
-            e.all_day = e.endDate - e.date >= 24*3600*1000;  // If start and end times 24 hours apart; assume an all day event
+            e.all_day = e.endDate - e.date >= 24 * 3600 * 1000 && new Date(e.date).getDay() != new Date(e.endDate).getDay();  // If start and end times 24 hours apart; assume an all day event
             e.expired = Date.now() > e.endDate;
             e.inProgress =  (e.date <= Date.now()) && (Date.now() <= e.endDate);
         } else {
