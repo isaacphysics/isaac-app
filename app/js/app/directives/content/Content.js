isaacphysics/isaac-app

commit e51bbac8c72b04d16210795f4746d8678ddeb9c0

Fix a shadowing issue (eslint:no-shadow)

app/js/app/directives/content/Content.js
@@ -61,9 +61,9 @@ define(['/partials/content/Content.html'], function(templateUrl) {
 
 			            			scope.$emit("ensureVisible");
 
-			            			var element = iElement.closest("dd");
-			            			if (null == element) {
-			            				element = iElement;
+			            			let dd = iElement.closest("dd");
+			            			if (null == dd) {
+			            				dd = iElement;
 			            			}
 
 									$timeout(function() {

commit 0f4a298588065126f8a4a855ea3fcbc21628cca8

Fix scroll-to-accordion section when location hash is present.
Caused by: e51bbac8c72b04d16210795f4746d8678ddeb9c0 (trying to fix shadowing)

app/js/app/directives/content/Content.js
@@ -69,7 +69,7 @@ define(['/partials/content/Content.html'], function(templateUrl) {
 									$timeout(function() {
 										$("html,body").animate({
 											// Use accordion section containing object, the 'dd' element:
-							                scrollTop: element.offset().top
+							                scrollTop: dd.offset().top
 							            });        
 									});
 			            		}