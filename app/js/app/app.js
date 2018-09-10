7987ce5aab2b5dd04d34a495bfca66e381ab3708
James Sharkey <jsharkey13@users.noreply.github.com>
2018-09-10 16:13:38 +0100

    Release v2.6.7

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -148,7 +148,7 @@ define([
             // Have reserved domians on ngrok.io, hardcode them for ease of use:
             apiProvider.urlPrefix("https://isaacscience.eu.ngrok.io/isaac-api/api");
         } else {
-            apiProvider.urlPrefix("/api/v2.6.1/api");
+            apiProvider.urlPrefix("/api/v2.6.2/api");
         }
 
         NProgress.configure({ showSpinner: false });
